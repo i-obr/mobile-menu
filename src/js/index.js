7f5bb86da65692c67b16affeaadf045448be5cd9
i-obr <obr.iv@ya.ru>
2017-11-15 16:14:59 +0300

    Update 2017-11-15T13:14:57.318Z

--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-import SideNavigation from './side-nav';
-
-const sidenav = new SideNavigation(document.querySelector('.side-nav'));
-
-document
-  .querySelector('.page-header__show-menu')
-  .addEventListener('click', sidenav.show);
